836bc6a7844eb1cb84828020f9820bec16ff8a83
mohammedal-ess <99287125+mohammedal-ess@users.noreply.github.com>
2022-03-30 14:39:57 -0400

    add code

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# created by mohammed al-essawi
+# created in march 2022
+# this program show "hello, world" in python3
+
+import ugame
+import stage 
+
+def game_scene ():
+    # this function is the main game game_scene
+    
+    print("\n\n\n") # 3 blank Lines 
+    print("Hello, world")
+    
+    # repeat forever,game loop
+    while True:
+        pass #just a place holder for now 
+      
+if __name__ == "__main__":
+    game_scene()
